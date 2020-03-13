059bb01137f1cd89b9aa37978360f0b4fabde2f5
rchuprina <40165367+rchuprina@users.noreply.github.com>
2020-03-13 14:29:00 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ int main()
     init_pair(7, COLOR_MAGENTA, COLOR_BLACK);
     curs_set(0);
 	keypad(stdscr, true);
+    mouseinterval(0);
     mousemask(ALL_MOUSE_EVENTS, NULL);
     noecho();
     scene();
